c45129c1d191eb7a9895baf15123d6e2c62df832
Usman Ghani <usmanghani39@gmail.com>
2020-11-05 06:22:46 +0000

    Update app.php

--- app.php
+++ app.php
@@ -1,9 +1,11 @@
 <?php
+
+if(isset($_POST['submit'])){
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-$email_form = 'usmanghani39@gmail.com';
+$email_from = 'usmanghani39@gmail.com';
 $email_subject = "New Form Submission"
 
 $email_body = "User Name: $name.\n".
@@ -18,5 +20,5 @@ $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: index.html");
-
+}
 ?>
