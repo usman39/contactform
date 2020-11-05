589786bd4fbeb57590e2c2383adaf17dcff2c1ba
Usman Ghani <usmanghani39@gmail.com>
2020-11-05 06:34:38 +0000

    Update app.php

--- app.php
+++ app.php
@@ -1,24 +1,26 @@
 <?php
 
 if(isset($_POST['submit'])){
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+  
 
-$email_from = 'usmanghani39@gmail.com';
-$email_subject = "New Form Submission"
+  $email_from = 'usmanghani39@gmail.com';
+  $email_subject = "New Form Submission"
 
-$email_body = "User Name: $name.\n".
-"User Email: $visitor_email.\n".
-"User Message: $message.\n";
+//   $email_body = "User Name: $name.\n".
+//   "User Email: $visitor_email.\n".
+//   "User Message: $message.\n";
 
-$to = "usmanghani39@gmail.com";
-$headers = "From: $email_form \r\n";
+  $mailto = "usmanghani39@gmail.com";
+  $headers = "From: ".$email_from;
+  $txt = "You have received an e-mail from " .$name.".\n\n".$message;
 
-$headers .= "Reply-To: $visitor_email \r\n";
+//   $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+  mail($mailto,$email_subject,$txt,$headers);
 
-header("Location: index.html");
+  header("Location: index.htm");
 }
 ?>
